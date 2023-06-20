ccee476f3660610f2f962705e7b0f107194d30b7
Ratnam Raju Bandi <ratnamrajubandi@gmail.com>
2023-06-20 13:15:13 +0530

    First Commit - Added Login Page

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import LoginComponent from "./components/login/LoginComponent";
+function App() {
+  return (
+    // <h1 className="text-3xl font-bold text-red-500 bg-gray-200 underline text-center">
+    //   Hello Digital Lync
+    // </h1>
+    <LoginComponent />
+  );
+}
+
+export default App;
